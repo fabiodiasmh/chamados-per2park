5d5f00b35e75558939be3d76f7f39b653a3124c4
Fabio Dias <84019663+Fabiodias10@users.noreply.github.com>
2025-11-10 00:11:00 -0300

    add swagger estilizado

--- src/main/java/com/chamados/Per2park/config/SwaggerConfig.java
+++ src/main/java/com/chamados/Per2park/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.chamados.Per2park.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,30 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
-                        .title("API - Per2Park Chamados")
-                        .version("1.0.0")
-                        .description("Documentação da API do sistema de chamados Per2Park"));
+                        .title("API Chamados")
+                        .version("1.0")
+                        .description("Documentação da API com autenticação JWT"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .description("Insira o token JWT obtido no login")));
     }
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .info(new Info()
+//                        .title("API - Chamados")
+//                        .version("1.0.0")
+//                        .description("Documentação da API do sistema de chamados Per2Park"));
+//    }
 }
